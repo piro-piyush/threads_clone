81d0c6b949bde704db9950db70c980a515c419f7
Piyush <piyush72717272@gmail.com>
2026-01-06 15:04:34 +0530

    feat: Load environment variables on startup
    Initializes and loads environment variables from the `.env` file when the application starts.
    
    This commit updates the `main` function to be asynchronous and ensures that `WidgetsFlutterBinding` is initialized before loading the environment variables using the `flutter_dotenv` package. This makes variables available throughout the app's lifecycle.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:thread_clone/routes/route_names.dart';
 import 'package:thread_clone/routes/routes.dart';
 import 'package:thread_clone/utils/theme/theme.dart';
 import 'package:thread_clone/views/home_view.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
