d5a57f339a4d889a37521c6d6a83228cd28e1d0f
Piyush <piyush72717272@gmail.com>
2026-01-08 16:59:47 +0530

    feat: Initialize Supabase and manage auth state
    This commit introduces initialization logic for the `SupabaseService` and adds a `SliverAppBarDelegate` for custom app bar behavior.
    
    - **SupabaseService Initialization:** Initializes the Supabase client using environment variables. It now tracks the current user's authentication state by listening to `onAuthStateChange` events, such as `signedIn` and `userUpdated`.
    - **User State Management:** Exposes the current `User` object and includes a method to update the user from a stored session.
    - **SliverAppBarDelegate:** Adds a new reusable widget, `SliverAppBarDelegate`, to create a persistent `TabBar` within a `CustomScrollView`.

--- lib/services/supabase_service.dart
+++ lib/services/supabase_service.dart
@@ -1,7 +1,37 @@
 import 'package:get/get.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:thread_clone/services/storage_service.dart';
+import 'package:thread_clone/utils/env.dart';
 
 class SupabaseService extends GetxService {
   static final SupabaseClient client = Supabase.instance.client;
-}
 
+  final Rx<User?> _currentUser = Rx<User?>(null);
+
+  User? get user => _currentUser.value;
+
+  @override
+  void onInit() async {
+    super.onInit();
+    await Supabase.initialize(url: Env.supabaseUrl, anonKey: Env.supabaseKey);
+    _currentUser.value = client.auth.currentUser;
+    listenAuthChanges();
+  }
+
+  // * first load the status
+  void updateUserFromSession() {
+    var session = Session.fromJson(StorageService.userSession!);
+    _currentUser.value = session?.user;
+  }
+
+  void listenAuthChanges() {
+    client.auth.onAuthStateChange.listen((state) {
+      final event = state.event;
+      if (event == AuthChangeEvent.userUpdated) {
+        _currentUser.value = state.session?.user;
+      } else if (event == AuthChangeEvent.signedIn) {
+        _currentUser.value = state.session?.user;
+      }
+    });
+  }
+}
---
+++ lib/views/profile/widgets/silver_app_bar_delegate.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  final TabBar _tabBar;
+
+  SliverAppBarDelegate(this._tabBar);
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(color: Colors.black, child: _tabBar);
+  }
+
+  @override
+  // TODO: implement maxExtent
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  // TODO: implement minExtent
+  double get minExtent => _tabBar.preferredSize.height;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
+  }
+}
